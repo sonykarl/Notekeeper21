ce96966ff1e886b62d381d380dd9c9d006d6ef10
sonykarl <sonykarl14@gmail.com>
2021-09-10 19:42:18 +0300

    added fab action

--- app/src/main/java/com/maxapps/viewmodelcodelabs/ui/DetailFragment.kt
+++ app/src/main/java/com/maxapps/viewmodelcodelabs/ui/DetailFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.maxapps.viewmodelcodelabs.R
 
 class DetailFragment : Fragment() {
 
+    private lateinit var fab: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +19,10 @@ class DetailFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_detail, container, false)
+        fab = view.findViewById(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            Navigation.createNavigateOnClickListener(R.id.action_detailFragment_to_addListFragment)
+        }
         return view
     }
  }
